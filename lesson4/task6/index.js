c858c5f5b880d8d69bfa7728af7a905184ba8f74
Mykola <k.maximov@gmail.com>
2022-01-20 18:19:37 +0200

    docs: update js for lesson4 task6

--- lesson4/task6/index.js
+++ lesson4/task6/index.js
@@ -4,16 +4,16 @@ let result = 0;
 for (let i = start; i <= 20; i += 1) {
   if (i % 5 === 0) {
     result = i;
-    break;
+    continue;
   } else if (i % 2 === 0 && i % 4 !== 0) {
     result += i;
-    break;
+    continue;
   } else if (i % 3 === 0) {
     result -= i;
-    break;
+    continue;
   } else if (i % 4 === 0) {
     result *= i;
-    break;
+    continue;
   }
 }
-console.log(result);
+
