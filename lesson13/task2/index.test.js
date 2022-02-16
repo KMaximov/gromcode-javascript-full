52008a768dfabfffab41cfe92190bb33f2c7b840
Mykola <k.maximov@gmail.com>
2022-02-16 23:12:26 +0200

    docs: update js for lesson13 task2

--- lesson13/task2/index.test.js
+++ lesson13/task2/index.test.js
@@ -3,6 +3,7 @@ import getSum, { getSquaredNumbers, getOddNumbers } from "./calculator.js";
 
 it("should get squared numbers", () => {
   const result = getSquaredNumbers([1, 2, 3]);
+
   expect(result).toEqual([1, 4, 9]);
 });
 
