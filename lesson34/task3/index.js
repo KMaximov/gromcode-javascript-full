f883893d69e765a1a90e4cc2e9eb7e953b4c3810
Mykola <k.maximov19@gmail.com>
2022-05-16 18:18:40 +0300

    docs: complete lesson34 task3

---
+++ lesson34/task3/index.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const userEmail = document.querySelector('#email');
+const userName = document.querySelector('[name = name]');
+const userPassword = document.querySelector('[name = password]');
+const createUserForm = document.querySelector('.login-form');
+const userRegisterBtn = document.querySelector('.submit-button');
+
+// const checkValid = () => {
+//     if (createUserForm.reportValidity()) {
+//         userRegister.disabled = false;
+//     }
+// };
+
+// createUserForm.addEventListener('input', checkValid); можна через input подію, тільки при першій валідації поля переходить на наступне
+
+createUserForm.addEventListener('change', () => {
+    if (createUserForm.reportValidity()) {
+        userRegisterBtn.disabled = false;
+    }
+});
+
+const newUser = userData => {
+    return fetch('https://627a4e1373bad50685866f2c.mockapi.io/api/v1/userForm', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8',
+        },
+        body: JSON.stringify(userData),
+    });
+};
+
+const registerNewUser = event => {
+    event.preventDefault();
+
+    const newUserData = {
+        name: userName.value,
+        password: userPassword.value,
+        email: userEmail.value,
+    };
+
+    newUser(newUserData)
+        .then(response => response.json())
+        .then(response => alert(JSON.stringify(response)))
+        .then(() => {
+            userName.value = '';
+            userPassword.value = '';
+            userEmail.value = '';
+        })
+        .then(() => {
+            userRegisterBtn.disabled = true;
+        });
+};
+
+userRegisterBtn.addEventListener('click', registerNewUser);
